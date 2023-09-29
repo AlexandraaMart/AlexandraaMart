d6d492e63c86ac3862b7fe0ffa3a71f31f76d070
AlexandraaMart <martynovaalex049@gmail.com>
2023-09-29 20:27:16 +0300

    add

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
+@dp.message(Command("answer"))
+async def cmd_answer(message: types.Message):
+    await message.answer("Это простой ответ")
+
+@dp.message(Command("reply"))
+async def cmd_reply(message: types.Message):
+    await message.reply('Это ответ с "ответом"')
+
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
