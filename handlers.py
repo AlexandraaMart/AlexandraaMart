4663c1697444b1957df8c272a6e6f18e52c5c28d
AlexandraaMart <martynovaalex049@gmail.com>
2023-09-30 19:17:55 +0300

    add

--- handlers.py
+++ handlers.py
@@ -8,10 +8,12 @@ router = Router()
 async def start_handler(msg: Message):
     await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение")
 
+@router.message(Command("end"))   #функция является обработчиком входящих сообщений
+async def end_handler(msg: Message):
+    await msg.answer("Пока! Если понадоблюсь, то я тут")
+
 @router.message() #реагирует на все сообщения, так как у него не задан ни один фильтр
 async def message_handler(msg: Message):
     await msg.answer(f"Твой ID: {msg.from_user.id}")
 
-@router.message(Command("end"))   #функция является обработчиком входящих сообщений
-async def end_handler(msg: Message):
-    await msg.answer("Пока! Если понадоблюсь, то я тут")
+
