4363e048faf7a7b52bc77798b46e4ecaed754de4
AlexandraaMart <martynovaalex049@gmail.com>
2023-09-30 21:32:51 +0300

    add

--- handlers.py
+++ handlers.py
@@ -2,6 +2,9 @@ from aiogram import types, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
 
+import kb
+import text
+
 router = Router()
 
 @router.message(Command("start"))   #функция является обработчиком входящих сообщений
@@ -17,3 +20,12 @@ async def message_handler(msg: Message):
     await msg.answer(f"Твой ID: {msg.from_user.id}")
 
 
+@router.message(Command("Menu"))
+async def menu_handler(msg: Message):
+    await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=kb.menu)
+
+@router.message(F.text == "Меню")
+@router.message(F.text == "Выйти в меню")
+@router.message(F.text == "◀️ Выйти в меню")
+async def menu(msg: Message):
+    await msg.answer(text.menu, reply_markup=kb.menu)
